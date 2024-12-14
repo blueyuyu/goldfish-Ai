c4d04ded35662012909b13b28e200489d2ca8d8c
blueyuyu <ublueyuyuyu@gmail.com>
2024-12-14 08:42:06 +0800

    🐳 chore(eslint And all files):
    修改eslint配置，修改项目换行符为LF, 增加配置，使打包项目css出现浏览器前缀

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import path from "path"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // 扩展默认配置而不是替换
 
-export default nextConfig;
+    // 例如，添加别名、插件或规则等
+
+    // 添加一个简单的示例：添加一个别名
+    config.resolve.alias["@components"] = path.join(__dirname, "components")
+
+    // 返回修改后的 Webpack 配置
+    return config
+  },
+}
+
+export default nextConfig
