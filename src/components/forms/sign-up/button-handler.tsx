4afa7b67af29c7f33568ddfe3ec17ce321a08fc7
chenyu <m8e-h9hsjrhej@dingtalk.com>
2025-02-26 18:10:07 +0800

    ✨ feat(button-handler): 追加了一些逻辑

--- src/components/forms/sign-up/button-handler.tsx
+++ src/components/forms/sign-up/button-handler.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useAuthContextHook } from "@/context/use-auth-context"
+import { useFormContext } from "react-hook-form"
 import React from "react"
+import { useSignUpForm } from "@/hooks/sign-up/use-sign-up"
 
 // 定义组件的属性类型
 type props = {}
@@ -10,7 +12,13 @@ const ButtonHandler = ({}: props) => {
   // 获取当前的步骤
   // name email password
   // 获取当前的步骤
-  const { currentStep } = useAuthContextHook()
+  const { currentStep, setCurrentStep } = useAuthContextHook()
+  const { formState, getFieldState, getValues } = useFormContext()
+  const { onGenerateOTP } = useSignUpForm()
+
+  const { isDirty: isName } = getFieldState("fullname", formState)
+  const { isDirty: isEmail } = getFieldState("email", formState)
+  const { isDirty: isPassword } = getFieldState("password", formState)
 
   return <div>aaads</div>
 }
