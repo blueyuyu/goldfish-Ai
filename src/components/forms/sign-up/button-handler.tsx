395de42fad782e92535d908e8b24f1e8b0a34df9
blueyuyu <ublueyuyuyu@gmail.com>
2025-03-03 08:33:57 +0800

    ✨ feat(env | ignore | button-handler): 修改env，增加button-handler按钮

--- src/components/forms/sign-up/button-handler.tsx
+++ src/components/forms/sign-up/button-handler.tsx
@@ -3,6 +3,8 @@ import { useAuthContextHook } from "@/context/use-auth-context"
 import { useFormContext } from "react-hook-form"
 import React from "react"
 import { useSignUpForm } from "@/hooks/sign-up/use-sign-up"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 // 定义组件的属性类型
 type props = {}
@@ -20,7 +22,65 @@ const ButtonHandler = ({}: props) => {
   const { isDirty: isEmail } = getFieldState("email", formState)
   const { isDirty: isPassword } = getFieldState("password", formState)
 
-  return <div>aaads</div>
+  if (currentStep === 1) {
+    return (
+      <div className="w-full flex flex-col gap-3 items-center">
+        <Button
+          type="submit"
+          className="w-full"
+          onClick={() => {
+            setCurrentStep((pre: number) => pre + 1)
+          }}
+        >
+          Continue
+        </Button>
+        <p>
+          Already have an account?
+          <Link href="/auth/sign-in" className="font-bold">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    )
+  }
+  if (currentStep === 2) {
+    return (
+      <div className="w-full flex flex-col gap-3 items-center">
+        <Button
+          type="submit"
+          className="w-full"
+          {...(isName &&
+            isEmail &&
+            isPassword && {
+              onClick: () => {
+                onGenerateOTP(getValues("email"), getValues("password"), setCurrentStep)
+              },
+            })}
+          onClick={() => {
+            setCurrentStep((pre: number) => pre + 1)
+          }}
+        >
+          continue
+        </Button>
+      </div>
+    )
+  }
+
+  if (currentStep === 3) {
+    return (
+      <div className="w-full flex flex-col gap-3 items-center">
+        <Button type="submit" className="w-full">
+          Create an account
+        </Button>
+        <p>
+          Already have an account?
+          <Link href="/auth/sign-in" className="font-bold">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    )
+  }
 }
 
 export default ButtonHandler
